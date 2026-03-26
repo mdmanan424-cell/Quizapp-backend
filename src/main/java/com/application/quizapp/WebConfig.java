aa24717e5f0b291d26cd0cfb3bbac57251f5a924
Bharath D <james@Bharaths-MacBook-Air.local>
2026-03-26 22:16:30 +0530

    update cors

--- src/main/java/com/application/quizapp/WebConfig.java
+++ src/main/java/com/application/quizapp/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://heartfelt-gumption-234c58.netlify.app/")
+                .allowedOrigins("https://quiz-master-17.netlify.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
